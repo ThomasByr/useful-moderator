7d468bdeeac994d6a4c1ac988567f0b43e08098f
Thomas Byr <tbouyer2000@gmail.com>
2023-04-18 15:18:43 +0200

    launch w/ python `useful-moderator.py`

--- useful-moderator.py
+++ useful-moderator.py
@@ -5,16 +5,23 @@ from src import UsefulClient, default_formatter, console_handler, log_lvl
 
 def get_token() -> str:
   return os.getenv('BOT_TOKEN')
+
+
 def get_prefix() -> str:
   return os.getenv('BOT_PREFIX', '!')
 
 
+def get_invite() -> str:
+  return os.getenv('BOT_INVITE')
+
+
 BOT_TOKEN = get_token()
 BOT_PREFIX = get_prefix()
+BOT_INVITE = get_invite()
 
 if __name__ == '__main__':
 
-  client = UsefulClient(BOT_PREFIX, help_command=None)
+  client = UsefulClient(BOT_PREFIX, BOT_INVITE, help_command=None)
   client.run(
     BOT_TOKEN,
     reconnect=True,
